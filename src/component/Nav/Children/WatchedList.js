609521c3f2931e6442e677c881b67e6d37f7ffde
AdeebAljerf <adeebaljerf2@gmail.com>
2024-10-24 14:19:40 +0300

    add netflix style

--- src/component/Nav/Children/WatchedList.js
+++ src/component/Nav/Children/WatchedList.js
@@ -4,7 +4,7 @@ export default function WatchedList() {
   return (
     <Link to="/watched" className="watchedList">
       <div className="watchlist-button">
-        <span>🎬</span>
+        {/* <span>🎬</span> */}
         <h2>My Watch List</h2>
       </div>
     </Link>
